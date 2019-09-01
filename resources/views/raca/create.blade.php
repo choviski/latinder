652aff994f71f734b7520d581d17cf0fe8e6ae87
FernandoFdeS <fernandofdes@hotmail.com>
2019-08-31 21:31:33 -0300

    tarefas 7-12

---
+++ resources/views/raca/create.blade.php
@@ -0,0 +1,23 @@
+@extends('../layouts/cruds')
+
+@section('header')
+    <h1 class="display-4">CADASTRO DE RAÇAS</h1>
+@endsection
+
+@section('content')
+    <form  action="{{Route('raca.store')}}" method="post">
+        @csrf
+        <div class="form-group bg-light p-2 rounded">
+            <label  for="nome">Nome:</label>
+            <input type="text" class="form-control" id="nome" placeholder="insira o nome da raça" name="nome" required>
+
+            <label  for="login">Id da espécie:</label>
+            <input type="text" class="form-control" id="id_especies" placeholder="Insira o id da raça" name="id_especies" required>
+
+            <input type="submit" class="btn btn-primary mt-2">
+
+        </div>
+    </form>
+
+
+@endsection
